04b0bb6b727e4be9ff88b525bdab37c2abf22bdc
kani81 <kaniarasiduraisamy.com>
2023-07-19 15:27:56 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,11 @@
+var arr=[11,10,70,75,59,2,4];
+sum=0;
+document.write("arr=[11,10,70,75,59,2,4]"+"<br>"+"<br>");
+for(let i=0;i<arr.length;i++){
+	sum=sum+arr[i];
+}
+		document.write(sum+"<br>"+"<br>");
+		document.write("average the number"+"<br>"+"<br>");
+		document.write(sum/arr.length+"<br>"+"<br>");
+		
+	
